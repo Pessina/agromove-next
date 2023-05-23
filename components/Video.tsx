d5fcfade1f4fe64dd8df3ca384c71ef5555bc88b
Felipe Pessina <fs.pessina@gmail.com>
2023-05-23 17:20:03 +0500

    Update packages and fix video responsive

--- components/Video.tsx
+++ components/Video.tsx
@@ -13,13 +13,26 @@ const Video: React.FC<VideoProps> = ({ title, id, className }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   return (
-    <div className={`${className} flex flex-col items-center gap-10`}>
+    <div
+      className={`${className} flex flex-col items-center gap-10 text-center`}
+    >
       <h2 className="text-3xl font-bold">{title}</h2>
-      <YouTube
-        videoId={id}
-        className="min-w-[100px] min-h-[200px] max-w-[90%]"
-        onReady={() => setIsLoading(false)}
-      />
+      <div className="flex-grow w-full">
+        <div className="relative pb-[56.25%]">
+          <YouTube
+            videoId={id}
+            className="absolute top-0 left-0 w-full h-full"
+            opts={{
+              width: "100%",
+              height: "100%",
+              playerVars: {
+                autoplay: 0,
+              },
+            }}
+            onReady={() => setIsLoading(false)}
+          />
+        </div>
+      </div>
       {isLoading && <LoaderSpinner />}
     </div>
   );
