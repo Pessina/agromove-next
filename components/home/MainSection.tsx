d48ed8138594ddca0470f290ec1a9ebb1c2f685a
Felipe Pessina <fs.pessina@gmail.com>
2023-08-31 18:07:25 +0600

    Update main section size

--- components/home/MainSection.tsx
+++ components/home/MainSection.tsx
@@ -14,7 +14,7 @@ type MainSectionProps = {
 const MainSection: React.FC<MainSectionProps> = ({ i18n, className }) => {
   return (
     <div
-      className={`${className} flex flex-col items-center justify-center text-center max-w-[800px] mx-auto gap-4 py-20`}
+      className={`${className} h-screen flex flex-col items-center justify-center text-center max-w-[800px] mx-auto gap-4 py-20`}
     >
       <h1 className="text-3xl md:text-5xl font-bold">{i18n.title}</h1>
       <p className="md:text-lg max-w-[85%]">{i18n.subtitle}</p>
